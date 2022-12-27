27523e80d7ac6a18b35ea1a06b851b3e1fcdaceb
hieupm <hieupm061299@gmail.com>
2022-12-27 10:01:17 +0700

    Initial commit

---
+++ src/main/java/com/demo/currencyexchangeservice/repo/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.demo.currencyexchangeservice.repo;
+
+import com.demo.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10001,'USD','VND',23642,'');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10002,'EUR','VND',25040,'');
+insert into CURRENCY_EXCHANGE
+(id, currency_from, currency_to, conversion_multiple, environment)
+values (10003,'AUD','VND',15842,'');
